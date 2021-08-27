9245cde55effde5b648954520b160f94899363d9
m-RNA <m-RNA@qq.com>
2021-08-27 21:49:43 +0800

    initial commit

--- RNA_UART/sys/baudrate_calculate.c
+++ RNA_UART/sys/baudrate_calculate.c
@@ -3,24 +3,32 @@
 /****************************************************/
 // MSP432P401R
 // 串口波特率计算
-// 版本 v1.0
 // Bilibili：m-RNA
 // E-mail:m-RNA@qq.com
-// 创建日期:2021/8/25
 /****************************************************/
 
-/* ************   说明   ************
+/******************************    说明    ******************************
  *
- * 源码为TI官方编写,本人只是将JS程序
- * 移植到了C语言平台,仅作为学习使用。
- * 源码出处为：http://software-dl.ti.com/msp430/msp430_public_sw/mcu/msp430/MSP430BaudRateConverter/index.html
+ * 源码为TI官方编写,本人只是将JS程序移植到了C语言平台,仅作为学习使用。源码出处为：
+ * http://software-dl.ti.com/msp430/msp430_public_sw/mcu/msp430/MSP430BaudRateConverter/index.html
  * 
- * v1.0 已知问题：
+ * ? 已知问题：
  * 调试时发现C语言的小数的大小与官方JS的不一样，
  * 导致了算出的UCSx(即secondModReg)不一样，
  * 这时如果出现不能准确传输时，请换一个波特率。
  *
- * ************   结束    ************/
+ * ? 需要注意：
+ * 波特率不能大于时钟频率，否则程序将会转到闪烁错误指示灯的死循环。
+ * 
+ * *****************************   版本说明   ******************************
+ * 
+ * ? v1.1  2021/8/27
+ * 添加支持固件库v3_21_00_05 
+ * 
+ * ? v1.0  2021/8/25
+ * 仅支持固件库v3_40_01_02
+ * 
+ * *******************************   结束    *******************************/
 
 /*
  *  ======== bitPosition ========
@@ -46,7 +54,11 @@ bool bitPosition(uint16_t value, uint16_t position)
  *  "Recommended Settings for Typical Crystals and Baudrates", of the
  *  MSP430FR57xx Family User's Guide (SLAU272A).
  */
-void eusci_calcBaudDividers(eUSCI_UART_ConfigV1 *uart_config, uint32_t baudRate)
+#ifdef EUSCI_A_UART_7_BIT_LEN
+void eusci_calcBaudDividers(eUSCI_UART_ConfigV1 *uart_config, uint32_t baudRate) //固件库v3_40_01_02
+#else
+void eusci_calcBaudDividers(eUSCI_UART_Config *uart_config, uint32_t baudRate) //固件库v3_21_00_05
+#endif
 {
     float maxAbsErrorInByte;
     float minAbsError;
--- RNA_UART/sys/uartinit.c
+++ RNA_UART/sys/uartinit.c
@@ -4,26 +4,30 @@
 /****************************************************/
 // MSP432P401R
 // 串口配置
-// 版本 v2.0
 // Bilibili：m-RNA
 // E-mail:m-RNA@qq.com
-// 创建日期:2021/8/25
 /****************************************************/
 
 /******************   版本更新说明   *****************
  * 
- * ? v2.0  2021/8/25
- * 函数名改名为：uart_init，并增添了波特率传入参数，
- * 可直接配置波特率，与正点原子封装代码风格统一。
- * 计算UART的代码单独打包为名为
- * baudrate_calculate的c文件和h文件，
  * ? 需要注意：
- * ①低频时钟频率下，高波特率使得传输时误差过大，
- * 比如35768Hz下19200波特率，
+ * ①低频时钟频率下，高波特率使得传输时误差过大,
+ * 比如35768Hz下19200波特率,
  * 会使得传输出错，这时可以尝试降低波特率。
  * ②baudrate_calculate的问题请去文件内查看。
  * 
- * ? v1.0 2021/8/11
+ * **************************************************
+ * 
+ * ? v2.1  2021/8/27
+ * 添加支持固件库v3_21_00_05 
+ * 
+ * ? v2.0  2021/8/25
+ * uart_init增添了波特率传入参数，可直接配置波特率。
+ * 计算UART的代码单独打包为名为
+ * baudrate_calculate的c文件和h文件,
+ * 
+ * ? v1.0 2021/7/17
+ * 仅支持固件库v3_40_01_02
  * 配置了SMCLK 48MHz 波特率 115200的初始化代码，
  * 对接标准输入输出库，使其能使用printf、scanf函数
  * 
@@ -45,7 +49,7 @@ int fgetc(FILE *f)
     return UART_receiveData(EUSCI_A0_BASE);
 }
 /*****************   函数说明   *****************
- * 
+ *
  * 以上两条对接标准输入输出库的函数:
  * int fputc(int ch, FILE *f);
  * int fgetc(FILE *f);
@@ -53,13 +57,15 @@ int fgetc(FILE *f)
  * 本人RNA，不是作者
  * 在此也表感谢
  * 使用时记得勾选魔术棒里的Use MicroLIB
- * 
+ *
  *****************   说明结束   *****************/
 
 /**************************************************************************************/
 
 void uart_init(uint32_t baudRate)
 {
+#ifdef EUSCI_A_UART_7_BIT_LEN
+    //固件库v3_40_01_02
     //默认SMCLK 48MHz 比特率 115200
     const eUSCI_UART_ConfigV1 uartConfig =
         {
@@ -75,11 +81,28 @@ void uart_init(uint32_t baudRate)
             EUSCI_A_UART_8_BIT_LEN                         // 8 bit data length
         };
     eusci_calcBaudDividers((eUSCI_UART_ConfigV1 *)&uartConfig, baudRate); //配置波特率
-
+#else
+    //固件库v3_21_00_05
+    //默认SMCLK 48MHz 比特率 115200
+    const eUSCI_UART_Config uartConfig =
+        {
+            EUSCI_A_UART_CLOCKSOURCE_SMCLK,                // SMCLK Clock Source
+            26,                                            // BRDIV = 26
+            0,                                             // UCxBRF = 0
+            111,                                           // UCxBRS = 111
+            EUSCI_A_UART_NO_PARITY,                        // No Parity
+            EUSCI_A_UART_LSB_FIRST,                        // MSB First
+            EUSCI_A_UART_ONE_STOP_BIT,                     // One stop bit
+            EUSCI_A_UART_MODE,                             // UART mode
+            EUSCI_A_UART_OVERSAMPLING_BAUDRATE_GENERATION, // Oversampling
+        };
+    eusci_calcBaudDividers((eUSCI_UART_Config *)&uartConfig, baudRate); //配置波特率
+#endif
     MAP_GPIO_setAsPeripheralModuleFunctionOutputPin(GPIO_PORT_P1, GPIO_PIN2 | GPIO_PIN3, GPIO_PRIMARY_MODULE_FUNCTION);
 
     /* Configuring UART Module */
     MAP_UART_initModule(EUSCI_A0_BASE, &uartConfig);
     /* Enable UART module */
-    MAP_UART_enableModule(EUSCI_A0_BASE);UART_enableModule(EUSCI_A0_BASE);
+    MAP_UART_enableModule(EUSCI_A0_BASE);
+    UART_enableModule(EUSCI_A0_BASE);
 }
