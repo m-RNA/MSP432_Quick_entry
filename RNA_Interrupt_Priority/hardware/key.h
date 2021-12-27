14bbe3988c6c33c32e6ad1ac86b2edad7bd76a3c
m-RNA <m-RNA@qq.com>
2021-12-27 12:03:36 +0800

    更新key驱动 支持位带操作

--- RNA_Interrupt_Priority/hardware/key.h
+++ RNA_Interrupt_Priority/hardware/key.h
@@ -24,12 +24,13 @@
 #define __KEY_H
 #include <ti/devices/msp432p4xx/driverlib/driverlib.h>
 
-#define KEY1 GPIO_getInputPinValue(GPIO_PORT_P1, GPIO_PIN1) //读取按键1
-#define KEY2 GPIO_getInputPinValue(GPIO_PORT_P1, GPIO_PIN4) //读取按键2
+#define KEY1 BITBAND_PERI(P1IN, 1) //读取按键1
+#define KEY2 BITBAND_PERI(P1IN, 4) //读取按键2
 
 #define KEY1_PRES 1 //KEY1按下
 #define KEY2_PRES 2 //KEY2按下
 
 void KEY_Init(void);       //IO初始化
 uint8_t KEY_Scan(uint8_t); //按键扫描函数
+
 #endif
