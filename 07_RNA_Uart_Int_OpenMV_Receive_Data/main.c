db6da0c0428fba92e951aa012f8c6f95795ea173
m-RNA <m-RNA@qq.com>
2022-07-19 12:56:58 +0800

    微调 修复小问题

--- 07_RNA_Uart_Int_OpenMV_Receive_Data/main.c
+++ 07_RNA_Uart_Int_OpenMV_Receive_Data/main.c
@@ -1,6 +1,6 @@
 /****************************************************/
 // MSP432P401R
-// 7 Uart中断 接收OpenMV数据
+// 7 Uart中断 接收OpenMV数据 参考示例
 // Bilibili：m-RNA
 // E-mail:m-RNA@qq.com
 // 创建日期:2022/07/18
@@ -16,8 +16,8 @@
 #define OPENMV_UART EUSCI_A2_BASE    // OpenMV使用串口
 #define OPENMV_Uart_RX_LENGTH_MAX 50 // OpenMV UART最大接收长度
 
-uint8_t OpenMV_Uart_Rx_Index = 0;                               // UART接收计数索引
-uint8_t OpenMV_Uart_Rx_Temp = 0;                                // UART中断接收缓存
+static uint8_t OpenMV_Uart_Rx_Index = 0;                        // UART接收计数索引
+static uint8_t OpenMV_Uart_Rx_Temp = 0;                         // UART中断接收缓存
 uint8_t OpenMV_Uart_Rx_Buffer[OPENMV_Uart_RX_LENGTH_MAX] = {0}; // UART接收缓存
 
 /*********   应用层   *********/
@@ -32,8 +32,8 @@ void OpenMV_Check_Data_Task(void)
     if (!OpenMV_Uart_Rx_Index) // 没有数据 退出
         return;
 
-    // 按需修改 （7：两个short； 5：一个short；依此类推）
-    if (OpenMV_Uart_Rx_Index < 7) // 接收长度是否达到
+    // 接收长度是否达到
+    if (OpenMV_Uart_Rx_Index < 7) // 按需修改 （7：两个short； 5：一个short；依此类推）
         return;
 
     // 检查格式是否正确
@@ -46,21 +46,21 @@ void OpenMV_Check_Data_Task(void)
         goto Send_Error;
 
     // 命令正确
-    OpenMV_Rx_Data_Analysis_State = 1; // 解析状态置1
     for (i = 0; i < ((OpenMV_Uart_Rx_Index - 3) >> 1); ++i)
     {
         OpenMV_Rx_Data[i] = OpenMV_Uart_Rx_Buffer[2 + (i << 1) + 1] << 8; // 高8位
         OpenMV_Rx_Data[i] |= OpenMV_Uart_Rx_Buffer[2 + (i << 1) + 0];     // 低8位
     }
+    OpenMV_Rx_Data_Analysis_State = 1; // 解析状态置1
 
     goto Jump; // 正常流程就跳过
 Send_Error:    // 解析错误
     // do something ...
 
-    printf("[Log] OpenMv Rx Data ERROR\r\n"); // 打印解析出错
-    printf_uart2("?");                        // 回复OpenMV "?"
-Jump:
+    printf_uart2("?"); // 回复OpenMV "?"
 
+    printf("[Log] OpenMv Rx Data ERROR\r\n"); // 打印解析出错
+Jump:
     // 清零接收缓冲 为下一次接收准备
     OpenMV_Uart_Rx_Index = 0;
     memset(OpenMV_Uart_Rx_Buffer, 0, OPENMV_Uart_RX_LENGTH_MAX);
@@ -70,16 +70,15 @@ Jump:
 void EUSCIA2_IRQHandler(void)
 {
     uint32_t status = MAP_UART_getEnabledInterruptStatus(OPENMV_UART);
-
     if (status & EUSCI_A_UART_RECEIVE_INTERRUPT_FLAG) //接收中断
     {
         OpenMV_Uart_Rx_Temp = MAP_UART_receiveData(OPENMV_UART); // 临时接收
-    }
 
-    if (OpenMV_Uart_Rx_Index < OPENMV_Uart_RX_LENGTH_MAX) // 接收缓冲是否溢出
-    {
-        OpenMV_Uart_Rx_Buffer[OpenMV_Uart_Rx_Index] = OpenMV_Uart_Rx_Temp; // 放入缓存
-        OpenMV_Uart_Rx_Index++;                                            // 索引加1
+        if (OpenMV_Uart_Rx_Index < OPENMV_Uart_RX_LENGTH_MAX) // 接收缓冲是否溢出
+        {
+            OpenMV_Uart_Rx_Buffer[OpenMV_Uart_Rx_Index] = OpenMV_Uart_Rx_Temp; // 放入缓存
+            OpenMV_Uart_Rx_Index++;                                            // 索引加1
+        }
     }
 }
 
